1fd1c5a391181be7144406912b65b938a6240d08
schakravarti <schakravarti930@gmail.com>
2020-11-01 14:07:41 +0530

    [Swapnil] Add : Add new contacts to the address book table

---
+++ UC3-InsertDataInTable.sql
@@ -0,0 +1,21 @@
+--Insert new contacts to address book
+INSERT INTO AddressBook
+VALUES
+( 
+    'Sue','Black','1000 Michigan Ave.','Chicago','IL',60605,'3889233120'
+),
+( 
+    'James','Blue','1000 Harbor Ave.','Seattle','WA',98116,'8332443923'
+),
+(
+    'Mike','Brown','360 Delmar Blvd.','New York','NY',10011,'8832322103'
+),
+(
+    'Liz','White','100 5th Ave.','New York','NY',10012,'8043232213'
+),
+(
+    'Meg','Stephenson','Stout St.','Denver','CO',80204,'7884440432'
+),
+(
+    'John','Gray','500 South St.','Chicago','IL',60605,'8334432122'
+);
