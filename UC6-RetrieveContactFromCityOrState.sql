d671cd98b59cc8a1644c67c434bece782031a091
schakravarti <schakravarti930@gmail.com>
2020-11-01 14:19:14 +0530

    [Swapnil] Add : Query to retrieve contact based on given city or state

---
+++ UC6-RetrieveContactFromCityOrState.sql
@@ -0,0 +1,7 @@
+--Retrieve Contact Belonging to a particular city
+SELECT * FROM AddressBook 
+WHERE city = 'Chicago';
+
+--Retrieve Contact Belonging to a particular state
+SELECT * FROM AddressBook 
+WHERE state = 'NY';
